fedc9c9b7563b4af6523cfa22db9950d38595f52
kkj53051000 <tkarnrwl7418@naver.com>
2022-09-29 16:52:43 +0900

    268 success

---
+++ 268_Missing Number.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int answer = 0;
+        
+        int maxValue = 0;
+        
+        if (nums.back() > nums.size()) {
+            maxValue = nums.back();
+        }else {
+            maxValue = nums.size();
+        }
+        
+        
+        for (int i = 0; i < maxValue; i++) {
+            if (nums[i] != i) {
+                return answer;
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+};
+
+//  정렬 -> maxValue 찾아서 반복문 -> 중간에 값없으면 리턴
