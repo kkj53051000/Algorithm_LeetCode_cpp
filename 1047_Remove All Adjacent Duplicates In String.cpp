49f81173a3b852cac697e6f9f95130347474c30c
kkj53051000 <tkarnrwl7418@naver.com>
2022-10-09 14:40:09 +0900

    vector => string

--- 1047_Remove All Adjacent Duplicates In String.cpp
+++ 1047_Remove All Adjacent Duplicates In String.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // 정답 넣을 배열
-        vector<char> arr;
-
+        
+        string answer;
+        
         for (int i = 0; i < s.size(); i++) {
-            if(arr.size() > 0) {
-                if (arr[arr.size()-1] == s[i]) { // arr 마지막 값과 현재값이 같으면 arr.pop()
-                    arr.pop_back();
-                }else { // 같지 않으면 arr.push()
-                    arr.push_back(s[i]);
+            if(answer.size() > 0) {
+                if (answer[answer.size()-1] == s[i]) { // arr 마지막 값과 현재값이 같으면 arr.pop()
+                    answer.pop_back();
+                }else {// 같지 않으면 arr.push()
+                    answer.push_back(s[i]);
                 }
             }else { // arr.size()가 0이면 arr.push()
-                arr.push_back(s[i]);
+                answer.push_back(s[i]);
             }
         }
         
-        
-        // arr => string
-        string str = "";
-        
-        for (int i = 0; i < arr.size(); i++) {
-            str += arr[i];
-        }
-        
-        return str;
+        return answer;
         
     }
 };
-
 
 // 성공
 
