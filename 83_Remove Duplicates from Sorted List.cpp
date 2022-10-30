55035b159894708ed9656453c788f560b972d283
kkj53051000 <tkarnrwl7418@naver.com>
2022-10-30 21:31:42 +0900

    83 success

---
+++ 83_Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        ListNode* dummy = head;
+        
+        while(dummy != nullptr && dummy->next != nullptr) {
+            if(dummy->val == dummy->next->val) { // 지금 값과 다음 값이 같을 때
+                dummy->next = dummy->next->next;  // 다음 주소에 다다음 주소를 넣어준다.
+            }else {
+                dummy = dummy->next;  // next
+            }
+        }
+        
+        return head;
+    }
+};
+
+// 성공
+
+// 시간 복잡도 : O(n)
