a5f6a101c5f1706a7e281e4aa35ffe903d571549
kkj53051000 <tkarnrwl7418@naver.com>
2022-11-02 10:17:39 +0900

    200 success

--- 83_Remove Duplicates from Sorted List.cpp
+++ 83_Remove Duplicates from Sorted List.cpp
@@ -18,7 +18,7 @@ public:
             if(dummy->val == dummy->next->val) { // 지금 값과 다음 값이 같을 때
                 dummy->next = dummy->next->next;  // 다음 주소에 다다음 주소를 넣어준다.
             }else {
-                dummy = dummy->next;  // next
+                dummy = dummy->next;  // next200. Number of Islands
             }
         }
         
